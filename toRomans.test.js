b1f7120dc54a5d280f4203ca1087da346a9e8aa5
cameronb150 <108947351+cameronb150@users.noreply.github.com>
2024-01-03 14:44:32 -0600

    Create toRomans.test.js

---
+++ toRomans.test.js
@@ -0,0 +1,21 @@
+const { toRoman } = require('./toRoman');
+
+describe('toRoman', () => {
+  // your code here.
+  
+  // TODO validate that the letters link
+  // to the direct number translations
+  test('todo', () => {
+    // Implement me
+  });
+
+  // TODO test that 0 is skipped
+  // assume that we get an empty string
+
+  // TODO test for negative numbers
+  // assume that we get an empty string
+
+  // TODO add one more test
+
+  // TODO add one more test
+});
